2ebeec6ea540a287c7cbbee7d06a496d1947ee45
palakkalsi <67413531+palakkalsi@users.noreply.github.com>
2024-05-17 22:28:58 +0530

    photoz-clone

---
+++ src/main/java/dev/palakkalsi/photozclone/PhotozCloneApplication.java
@@ -0,0 +1,13 @@
+package dev.palakkalsi.photozclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotozCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotozCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/palakkalsi/photozclone/PhotozCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.palakkalsi.photozclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotozCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
